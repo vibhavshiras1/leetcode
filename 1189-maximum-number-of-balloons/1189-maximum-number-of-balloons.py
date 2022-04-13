b96fb30b25cf08970d908b657484da64f1b96af0
vibhavshiras1 <60001137+vibhavshiras1@users.noreply.github.com>
2022-04-13 14:18:45 +0530

    Time: 26 ms (73.15%), Space: 14.1 MB (6.48%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         count = 0
         
-        dict1 = {}
+        dict1 = {'b':0,'a':0,'l':0,'o':0,'n':0}
         
         for i in range(len(text)):
             if(text[i] not in dict1):
@@ -18,7 +18,7 @@ class Solution(object):
         
         idx = 0
         
-        while(idx<len(str1)):
+        '''while(idx<len(str1)):
             if(str1[idx] in dict1):
                 dict1[str1[idx]] -= 1
                 if(dict1[str1[idx]]==0):
@@ -30,5 +30,16 @@ class Solution(object):
                 count += 1
                 idx = 0
                 
-        return count
+        return count'''
+        arr = []
+        arr.append(dict1['b'] // 1)
+        arr.append(dict1['a'] // 1)
+        arr.append(dict1['o'] // 2)
+        arr.append(dict1['n'] // 1)
+        arr.append(dict1['l'] // 2)
+        
+        return min(arr)
+        
+        
+        
         
