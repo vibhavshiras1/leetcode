2f996f6e8cc4b567d3d9b05300bf898ebd079ad6
vibhavshiras1 <60001137+vibhavshiras1@users.noreply.github.com>
2022-04-17 12:51:40 +0530

    Time: 31 ms (96.66%), Space: 15.4 MB (8.99%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -5,12 +5,28 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        s = ''.join(sorted(s))
-        t = ''.join(sorted(t))
-        
-        if(s==t):
-            return True
-        else:
+        if(len(s)!=len(t)):
             return False
+        
+        dict1 = {}
+        
+        for i in range(len(s)):
+            if(s[i] not in dict1):
+                dict1[s[i]] = 1
+            else:
+                dict1[s[i]] += 1
+            
+            if(t[i] not in dict1):
+                dict1[t[i]] = -1
+            else:
+                dict1[t[i]] -= 1
+                
+        val = dict1.values()
+        
+        for i in range(len(val)):
+            if(val[i]!=0):
+                return False
+        
+        return True
             
         
