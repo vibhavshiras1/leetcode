ddbbc794b4f8b65e76aa55258cac612de703e0b1
vibhavshiras1 <60001137+vibhavshiras1@users.noreply.github.com>
2022-04-25 11:16:50 +0530

    Time: 24 ms (41.03%), Space: 13.4 MB (58.97%) - LeetHub

---
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findOcurrences(self, text, first, second):
+        """
+        :type text: str
+        :type first: str
+        :type second: str
+        :rtype: List[str]
+        """
+        res = []
+        words = text.split(' ')
+        count = 0
+        i = 0
+        s = first + second
+        
+        for i in range(len(words)-2):
+            w = words[i] + words[i+1]
+            if(w==s):
+                res.append(words[i+2])
+        
+        return res
+                
+            
+        
